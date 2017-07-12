6c94777310c91dbd477371f3211ada40f35a715a
Takalani Maposa <Takalani@Afrigis.local>
2017-07-12 14:38:40 +0200

    ilse-extensions javadoc for methods getCode, getMessage, getSource for census, postal code & suburb risk profile

--- search-extension/src/main/java/com/afrigis/services/search/extension/CensusResponse.java
+++ search-extension/src/main/java/com/afrigis/services/search/extension/CensusResponse.java
@@ -14,9 +14,24 @@ public interface CensusResponse extends Response {
 
     public Census getResult();
 
+    /**
+     * AfriGIS service call response, indicates status of service call
+     *
+     * @return code
+     */
     public int getCode();
 
+    /**
+     * AfriGIS service call response message
+     *
+     * @return message from AfriGIS service call, null if request success
+     */
     public String getMessage();
 
+    /**
+     * AfriGIS service call resource used
+     *
+     * @return name of API method used in AfriGIS services
+     */
     public String getSource();
 }
